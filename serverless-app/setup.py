2829f2a9612aaa4722b5d3cc285ad6d368b6668e
Christina Or <christinaor.tech@gmail.com>
2025-07-07 00:15:46 -0400

    Update setup

--- serverless-app/setup.py
+++ serverless-app/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='py_lambda_package',
     version='0.1.0',
-    packages=find_packages(),  # Automatically finds packages like db/, utils/, etc.
+    packages=find_packages(),
     install_requires=[
         'boto3',
         'botocore'
